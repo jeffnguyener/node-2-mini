9470ba58a13b787720bbc50487a867cc2394a3a2
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-05-06 13:32:14 -0600

    update v1.1

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bc = require('./controllers/books_controller')
+
+const app = express();
+app.use(express.json());
+app.use(express.static(__dirname + '/..build'));
+
+app.get('/api/books', bc.read);
+app.post('/api/books', bc.create);
+app.put('/api/books/:id', bc.update);
+app.delete('/api/books/:id', bc.delete);
+
+app.listen(3000, () => {
+    console.log('Listening on port 4000')
+});
+
